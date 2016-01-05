f37c14372ad09f3cf7513269d91f09a8d0948542
Bill Pliske <bpliske@azcentral.com>
2016-01-04 17:41:06 -0700

    added PNGs and comments to resize.sh file

--- resize.sh
+++ resize.sh
@@ -1,6 +1,30 @@
+# This makes the directories — the actual resize.sh file should be sitting in the project folder
 mkdir -p images/output_600
 mkdir -p images/output_768
 mkdir -p images/output_993
+
+######################
+# JPGs are done here
+######################
+
+# This block of code updates JPGs in the images folder, and outputs to the 600 folder
 mogrify -path images/output_600 -filter Triangle -define filter:support=2 -thumbnail 600 -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 72 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB images/*.jpg
+
+# This block of code updates JPGs in the images folder, and outputs to the 768 folder
 mogrify -path images/output_768 -filter Triangle -define filter:support=2 -thumbnail 768 -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 72 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB images/*.jpg
+
+# This block of code updates JPGs in the images folder, and outputs to the 993 folder
 mogrify -path images/output_993 -filter Triangle -define filter:support=2 -thumbnail 993 -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 72 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB images/*.jpg
+
+######################
+# PNGs are done here
+######################
+
+# This block of code updates PNGs in the images folder, and outputs to the 600 folder
+mogrify -path images/output_600 -filter Triangle -define filter:support=2 -thumbnail 600 -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 72 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB images/*.png
+
+# This block of code updates PNGs in the images folder, and outputs to the 768 folder
+mogrify -path images/output_768 -filter Triangle -define filter:support=2 -thumbnail 768 -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 72 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB images/*.png
+
+# This block of code updates PNGs in the images folder, and outputs to the 993 folder
+mogrify -path images/output_993 -filter Triangle -define filter:support=2 -thumbnail 993 -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 72 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB images/*.png
